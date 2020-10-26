d96012933b14ca61878cf01c6b6d5bca29881d7d
Bartek Popielarczyk <popielarczykbartek@gmail.com>
2020-10-26 19:46:11 +0100

    Dodanie mozliwosci edycji samochodu

--- Komis/Controllers/SamochodController.cs
+++ Komis/Controllers/SamochodController.cs
@@ -1,4 +1,5 @@
-﻿using Komis.Models;
+﻿using AspNetCore;
+using Komis.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -42,8 +43,27 @@ namespace Komis.Controllers
                 return RedirectToAction(nameof(Index)); //nameof(Index) to to samo co "Index"
             }
             return View(samochod);
+        }
 
+        public IActionResult Edit(int id)
+        {
+            var samochod = _samochodRepository.PobierzSamochodOId(id);
+                if (samochod == null)
+                return NotFound();
 
+            return View(samochod);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Samochod samochod)
+        {
+            if (ModelState.IsValid)
+            {
+                _samochodRepository.EdytujSamochod(samochod);
+                return RedirectToAction(nameof(Index)); 
+            }
+            return View(samochod);
         }
 
     }
